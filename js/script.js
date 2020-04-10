b1575e153c416fb5979a8cb1543cff75ffa0f898
teneolepos <siningzhou@gmail.com>
2020-04-10 16:28:52 -0400

    Index updates

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(function() {
 /*-------------------------------------------
 Load Page
 ---------------------------------------------*/
-		
+
 	$('body').waitForImages({
 		finished: function() {
 				Website();
@@ -21,24 +21,24 @@ Ajax link page transitions
 	$("a.ajax-link").live("click", function(){
 		$this = $(this);
 		var link = $this.attr('href');
-		var current_url = $(location).attr('href');	
-		
-		if( link != current_url && link != '#' ) { 
+		var current_url = $(location).attr('href');
+
+		if( link != current_url && link != '#' ) {
 		$.ajax({
 			url:link,
-			processData:true, 
-			dataType:'html', 
+			processData:true,
+			dataType:'html',
 			success:function(data){
-				document.title = $(data).filter('title').text(); 
+				document.title = $(data).filter('title').text();
 				current_url = link;
         if (typeof history.pushState != 'undefined') history.pushState(data, 'Page', link);
-        
-          setTimeout(function(){						
+
+          setTimeout(function(){
           $('#preloader').delay(50).fadeIn(600);
-          $('html, body').delay(1000).animate({ scrollTop:  0  },1000);						
-					
+          $('html, body').delay(1000).animate({ scrollTop:  0  },1000);
+
 					setTimeout(function(){
-							
+
             $('#ajax-content').html($(data).filter('#ajax-content').html());
             $('#ajax-sidebar').html($(data).filter('#ajax-sidebar').html());
 
@@ -47,9 +47,9 @@ Ajax link page transitions
 								Website();
 								backLoading();
 								$('.opacity-nav').delay(50).fadeOut(600);
-              },										
+              },
               waitForAll: true
-						});								
+						});
 					},1000);
 					},0);
 			}
@@ -64,29 +64,29 @@ When you click back arrow
 ---------------------------------------------*/
 
 
-function backLoading() {  
+function backLoading() {
     $(window).on("popstate", function () {
         $('body').fadeOut('slow',function(){
             location.reload();
         });
         $('body').fadeIn();
     });
-}   
+}
 
 /*-------------------------------------------
 Load Page - next Open Site
 ---------------------------------------------*/
 
 function Website() {
-		CheckScripts();		
+		CheckScripts();
 		Masonry();
 		$('body').jKit();
 		backgroundmenu();
 		setTimeout(function(){
-			$(".preloader").fadeOut(500);							
+			$(".preloader").fadeOut(500);
 		},2000);
 		setTimeout(function(){
-			$('header').fadeIn();							
+			$('header').fadeIn();
 		},500);
 }
 
@@ -112,7 +112,7 @@ Masonry Check Script
 
 function Masonry() {
        var $container = $('.portfolio-grid');
-     
+
        $container.imagesLoaded( function(){
          $container.masonry({
            itemSelector : 'li'
@@ -136,9 +136,9 @@ function backgroundmenu() {
             } else {
                 $('header').fadeIn();
             }
-        }); 
+        });
      }
-     
+
      if($("#header-white").length) {
 
          $(window).scroll(function(){
@@ -149,10 +149,10 @@ function backgroundmenu() {
             } else {
                 $('header').css( "background", "none" );
             }
-        }); 
+        });
      }
 
-   
+
   });
 
 }
@@ -168,7 +168,7 @@ $(document).ready(function(){
 	setTimeout( function(){
 		if($("#site-type").length) {
         $(".typewrite span").typed({
-            strings: ["show case ", "projects "],
+            strings: ["Product Manager", "Public health advocate", "Seeker of food adventures"],
             typeSpeed: 100,
             backDelay: 500,
             loop: false,
@@ -202,7 +202,7 @@ function sidebarhero() {
         }
         fading.css('opacity',opacity);
     });
-  } 
+  }
 }
 
 
@@ -240,6 +240,113 @@ function hidePreloader() {
   $(".preloader").delay(2000).fadeOut("slow");
 }
 
+/*-------------------------------------------
+Single Page JS
+---------------------------------------------*/
+
+  // niceScroll
+  $("html").niceScroll();
+
+
+  // Stick menu
+  $(".menu").sticky({topSpacing:0});
+
+
+
+
+  // Menu Scroll to content and Active menu
+  var lastId,
+    topMenu = $("#menu"),
+    topMenuHeight = topMenu.outerHeight()+145,
+    menuItems = topMenu.find("a"),
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+   $('a[href*=#]').bind('click', function(e) {
+	e.preventDefault();
+
+	var target = $(this).attr("href");
+
+
+	$('html, body').stop().animate({ scrollTop: $(target).offset().top-140 }, 1000, function() {
+
+	});
+
+	return false;
+   });
+
+  $(window).scroll(function(){
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+
+   if (lastId !== id) {
+       lastId = id;
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }
+  });
+
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+
+    $(".footer").css( "position", "relative" );
+    $(".contact").css( "marginBottom", "0" );
+
+}
+else
+{
+
+  // FadeTo elements
+  if ( $(window).width() > 1023) {
+
+    tiles = $("h2, h3, .column-one, .column-two, .column-three, .grid li, .contact .content .form, .contact .content .contact-text ").fadeTo(0, 0);
+
+    $(window).scroll(function(d,h) {
+      tiles.each(function(i) {
+          a = $(this).offset().top + $(this).height();
+          b = $(window).scrollTop() + $(window).height();
+          if (a < b) $(this).fadeTo(1000,1);
+      });
+    });
+
+  }
+
+}
+
+
+
+  //Menu mobile click
+  $( ".icon" ).click(function() {
+    $( " ul.menu-click" ).slideToggle( "slow", function() {
+    // Animation complete.
+    });
+  });
+
+
+$(window).load(function(){
+
+$(".preloader").delay(1000).fadeOut("slow")
+
+  // Parallax
+  if ($('.parallax-background').length) {
+    $(".parallax-background").parallax();
+  }
+
+  // Parallax
+  if ($('.parallax-background-partners').length) {
+    $(".parallax-background-partners").parallax();
+  }
+
+});
 
 
 })//End
